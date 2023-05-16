d006920aa6136633988ad8dd266dce3da875e064
Samuel Azcona <sazcona@itbit.com>
2023-05-16 04:23:40 -0300

    update

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile, tools
 from conan.tools.scm import Git
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.files import get
 
 class mysql_connector_cxxRecipe(ConanFile):
     name = "libmysqlcppconn"
-    version = "8.0.33"
     license = "GPL-2.0"
-    author = "Samuel Aazcona <samuel@theopencompany.dev>"
+    author = "Samuel Aazcona <samuel@gmail.com>"
     url = "https://github.com/samuaz/libmysqlcppconn-conan"
     homepage = "https://dev.mysql.com/doc/connector-cpp/8.0"
     description = "A Conan package for MySQL Connector/C++ with OpenSSL, Boost, and libmysqlclient"
@@ -17,14 +17,11 @@ class mysql_connector_cxxRecipe(ConanFile):
     requires = ("boost/1.81.0", "openssl/1.1.1t", "libmysqlclient/8.0.31")
 
     def source(self):
-        git = Git(self)
-        git.clone(url="https://github.com/mysql/mysql-connector-cpp.git", target=".")
-        # Please, be aware that using the head of the branch instead of an immutable tag
-        # or commit is not a good practice in general
-        git.checkout("8.0.33")
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def layout(self):
-        cmake_layout(self)
+        file_name = "mysql-connector-c++"
+        cmake_layout(self, src_folder=f"{file_name}-{self.version}-src")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -58,10 +55,3 @@ class mysql_connector_cxxRecipe(ConanFile):
         self.cpp_info.system_libs = ["resolv"]
         self.cpp_info.libdirs = ["lib","lib64"]
         self.cpp_info.includedirs = ["include"]
-
-"""     def package_info(self):
-        self.cpp_info.libs = ["mysqlcppconn"]
-        self.cpp_info.system_libs = ["libmysqlcppconn8"]
-        # conan sets libdirs = ["lib"] and includedirs = ["include"] by default
-        self.cpp_info.libdirs = ["lib"]
-        self.cpp_info.includedirs = ["include"]            """
